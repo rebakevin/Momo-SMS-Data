078f905c743f57cec3ba69b76513c1a9bdcf2846
V-Mugisha <m.victor@alustudent.com>
2026-01-22 00:22:44 +0200

    Add authentication middleware to SMSTransactionsHandler

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 from api.auth import is_authenticated
 
 class SMSTransactionsHandler(BaseHTTPRequestHandler):
+    
     def _send_error_response(self, status_code, message):
         self.send_response(status_code)
         self.send_header('Content-type', 'application/json')
@@ -14,3 +15,12 @@ class SMSTransactionsHandler(BaseHTTPRequestHandler):
         response = {"error": message}
 
         self.wfile.write(json.dumps(response).encode())
+
+    def do_GET(self):
+        # This acts like an authentication middleware
+        if not is_authenticated(self.headers):
+            self._send_error_response(
+                401, "Unauthorized: Invalid or missing credentials")
+            return
+
+        # Continue with your logic here...
